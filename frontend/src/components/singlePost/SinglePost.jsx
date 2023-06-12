623f803e45913611f36d58e82a9d61c86f33db09
jay-kishan010 <124438245+jay-kishan010@users.noreply.github.com>
2023-06-12 18:28:52 +0530

    ..

--- frontend/src/components/singlePost/SinglePost.jsx
+++ frontend/src/components/singlePost/SinglePost.jsx
@@ -12,11 +12,16 @@ const SinglePost = () => {
   const location=useLocation()
 const path=location.pathname.split("/")[2];
 const [post,setPosts]=useState({})
+const [title,setTitle]=useState("");
+const [desc,setDesc]=useState("");
+const [updateMode,setUpdateMode]=useState(false);
 const PF="http://localhost:5000/images/";
 useEffect(()=>{
   const getPost=async()=>{
     const res=await axios.get("http://localhost:5000/api/posts/"+path);
    setPosts(res.data);
+   setTitle(res.data.title)
+   setDesc(res.data.desc)
   }
   getPost();
 },[path])
@@ -30,6 +35,24 @@ const handleDelete=async()=>{
   } catch (error) {
     
   }
+};
+
+const handleUpdate=async()=>{
+  try {
+    
+    await axios.put(`http://localhost:5000/api/posts/${post._id}`,{
+     
+        
+        username:user.username,
+        
+        title,
+        
+        desc
+    })
+    setUpdateMode(false); 
+  } catch (error) {
+    console.log(error)
+  }
 }
   return (
     <div className="singlePost">
@@ -42,18 +65,24 @@ const handleDelete=async()=>{
         alt=""
         />
         )}
-        <h1 className="singlePostTitle">
-        {post.title}
-        {post.username === user?.username && (
+        {
+          updateMode ? <input type="text" value={title} className="singlePostTitle" autoFocus onChange={(e)=>setTitle(e.target.value)}/>:(
 
+
+            <h1 className="singlePostTitle">
+        {title}
+        {post.username === user?.username && (
+          
           <div className="singlePostEdit">
             
-            <FiEdit style={{color:"green"}}/> 
+            <FiEdit style={{color:"green"}} onClick={()=>setUpdateMode(true)}/> 
             <BsTrash style={{color:"red",marginLeft:"10px"}}  onClick={handleDelete}/>
            
           </div>
         )}
         </h1>
+            )
+          }
         <div className="singlePostInfo">
           <span>
             Author:
@@ -65,9 +94,21 @@ const handleDelete=async()=>{
           </span>
           <span>{new Date(post.createdAt).toDateString()}</span>
         </div>
+        {
+          updateMode ? (
+            <textarea value={desc} className="singlePostDesc" onChange={(e)=>setDesc(e.target.value)}/>
+          ) :(
+
         <p className="singlePostDesc">
-        {post.desc}
+        {desc}
         </p>
+          )
+        }
+      {
+        updateMode && (
+          <button className="singlePostButton" onClick={handleUpdate}>Update</button>
+        )
+      }
       </div>
     </div>
   )
